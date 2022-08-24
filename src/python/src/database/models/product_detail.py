3fd0c462cff721048e402c78e4452a70742e1e02
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2022-08-24 13:43:21 +0300

    fix(database): fix product detail model

--- src/python/src/database/models/product_detail.py
+++ src/python/src/database/models/product_detail.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from operator import index
 from sqlalchemy import Column
 from sqlalchemy.dialects.mysql import VARCHAR, TEXT, FLOAT, BIGINT
 
@@ -11,13 +10,13 @@ class ProductDetail(Base, MysqlPrimaryKeyMixin, MysqlStatusMixin, MysqlTimestamp
     __tablename__ = "product_details"
 
     url = Column("url", VARCHAR(768), unique=True, nullable=False)
-    external_id = Column("external_id", BIGINT(unsigned=True), nullable=False)
+    external_id = Column("external_id", BIGINT(unsigned=True), nullable=True)
     brand = Column("brand", VARCHAR(768), index=True, nullable=False)
     model = Column("model", VARCHAR(768), index=True, nullable=False)
     category = Column("category", VARCHAR(768), index=True, nullable=False)
-    name = Column("name", VARCHAR(768), index=True, nullable=False)
+    name = Column("name", VARCHAR(768), index=True, nullable=True)
     images = Column("images", TEXT(), nullable=True)
-    price = Column("price", FLOAT(), index=True, nullable=False)
+    price = Column("price", FLOAT(), index=True, nullable=True)
     parameters = Column("parameters", TEXT(), nullable=True)
     description = Column("description", TEXT(), nullable=True)
     exception = Column("exception", TEXT(), nullable=True)
